04aeb61835799a7c4ae428a2f4c788c6abc78347
unknown <asatulsinha4@gmail.com>
2022-03-04 19:41:09 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Login_SignUp from './pages/Login-SignUp/Login_SignUp';
 import BackdropFilter from './Test/BackdropFilter/BackdropFilter';
+import {Routes as Switch, Route} from 'react-router-dom';
+import Page404 from './pages/Page404';
 
 function App() {
   return (
     <>
-    <Login_SignUp />
     {/* <BackdropFilter /> */}
+    <Switch>
+      <Route path='' element={<Login_SignUp />} />
+      <Route path='*' element={<Page404 />} />
+    </Switch>
     </>
   );
 }
